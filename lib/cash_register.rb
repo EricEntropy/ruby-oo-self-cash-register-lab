044b42b75026d3d63221959b24e94bec88f983e6
Eric <contreras.eric18@gmail.com>
2021-01-31 00:44:09 -0500

    commit tree 41a6c7cbf7a347845aa9baa4bae458e823f41417

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+class CashRegister
 
+    attr_accessor :total, :discount, :item_list, :item, :price, :quantity
+
+    def initialize(discount = 0)
+        @total = 0 
+        @item_list = []
+        @discount = discount
+    end 
+
+    def add_item(item, price, quantity = 1.0)
+        @item = item
+        @price = price 
+        @quantity = quantity
+        #add items to list based on quantity
+        i = 0
+        while i < quantity
+            item_list << item
+            i += 1
+        end
+        #update total
+        self.total = total + price*quantity
+    end 
+
+    def apply_discount
+        if @discount > 0
+            #math line to find amount off, given discount
+            total_discount = 1 - @discount.to_f/100.0
+            self.total = total * total_discount
+            return "After the discount, the total comes to $#{total.to_int}."
+        else 
+            return "There is no discount to apply."
+        end
+    end 
+
+    def items 
+        self.item_list
+    end 
+
+    def void_last_transaction
+        self.total = total - price.to_f * quantity.to_f
+        item_list.shift(item_list.length)
+    end 
+end 
